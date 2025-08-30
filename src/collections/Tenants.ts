a65bfac0c3e4378bcc47eecd4d5cea667d39be64
omkar <omkarpadalkar21@gmail.com>
2025-08-30 11:27:14 +0530

    Add Mutli Tenancy Plugin

---
+++ src/collections/Tenants.ts
@@ -0,0 +1,53 @@
+import type { CollectionConfig } from "payload";
+
+export const Tenants: CollectionConfig = {
+  slug: "tenants",
+  admin: {
+    useAsTitle: "subdomain",
+  },
+  auth: true,
+  fields: [
+    {
+      name: "name",
+      required: true,
+      type: "text",
+      label: "Store Name",
+      admin: {
+        description: "This is the name of the store (e.g. Farm Fresh Stores)",
+      },
+    },
+    {
+      name: "subdomain",
+      type: "text",
+      index: true,
+      required: true,
+      unique: true,
+      admin: {
+        description:
+          "This is the subdomain for the store (e.g. [subdomain].harvestly.com",
+      },
+    },
+    {
+      name: "image",
+      type: "upload",
+      relationTo: "media",
+    },
+    {
+      name: "stripeAccountId",
+      type: "text",
+      required: true,
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      name: "stripeDetailsSubmitted",
+      type: "checkbox",
+      admin: {
+        readOnly: true,
+        description:
+          "You cannot create products until you submit your Stripe details",
+      },
+    },
+  ],
+};
