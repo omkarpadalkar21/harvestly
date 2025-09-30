1bae7f9cf53014365e1cf69601ef8a3ad6bb0e2b
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-09-30 09:40:42 +0530

    Add Stripe setup

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -1,15 +1,75 @@
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcuedures
+} from "@/trpc/init";
 import { z } from "zod";
 
 import { Media, Tenant } from "@/payload-types";
 import { TRPCError } from "@trpc/server";
+import type Stripe from "stripe";
 
 export const checkoutRouter = createTRPCRouter({
+  purchase: protectedProcuedures
+    .input(
+      z.object({
+        productIds: z.string().min(1),
+        tenantSubdomain: z.string().min(1)
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const products = await ctx.db.find({
+        collection: "products",
+        depth: 2,
+        where: {
+          and: [
+            {
+              id: {
+                in: input.productIds
+              }
+            },
+            {
+              "tenant.subdomain": {
+                equals: input.tenantSubdomain
+              }
+            }
+          ]
+        }
+      });
+
+      if (products.totalDocs !== input.productIds.length) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Products not found"
+        });
+      }
+
+      const tenantsData = await ctx.db.find({
+        collection: "tenants",
+        limit: 1,
+        pagination: false,
+        where: {
+          subdomain: {
+            equals: input.tenantSubdomain
+          }
+        }
+      });
+
+      const tenants = tenantsData.docs[0];
+      if (!tenants) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "No tenants found" });
+      }
+
+      // TODO: Throw error if stripe details not submitted
+
+      // const lineItems = Stripe.Checkout.SessionCreateParams.LineItem[];
+
+    }),
   getProducts: baseProcedure
     .input(
       z.object({
-        ids: z.array(z.string()),
-      }),
+        ids: z.array(z.string())
+      })
     )
     .query(async ({ ctx, input }) => {
       const data = await ctx.db.find({
@@ -17,15 +77,15 @@ export const checkoutRouter = createTRPCRouter({
         depth: 2,
         where: {
           id: {
-            in: input.ids,
-          },
-        },
+            in: input.ids
+          }
+        }
       });
 
       if (data.totalDocs !== input.ids.length) {
         throw new TRPCError({
           code: "NOT_FOUND",
-          message: "Product not found",
+          message: "Product not found"
         });
       }
 
@@ -40,8 +100,8 @@ export const checkoutRouter = createTRPCRouter({
         docs: data.docs.map((doc) => ({
           ...doc,
           image: doc.image as Media | null,
-          tenant: doc.tenant as Tenant & { image: Media | null },
-        })),
+          tenant: doc.tenant as Tenant & { image: Media | null }
+        }))
       };
-    }),
+    })
 });
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,7 +1,9 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { getPayload } from "payload";
 import { cache } from "react";
 import config from "@payload-config";
+import { headers as getHeaders } from "next/headers";
+
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -27,4 +29,26 @@ export const baseProcedure = t.procedure.use(async ({ next }) => {
   });
 
   return next({ ctx: { db: payload } });
+});
+
+export const protectedProcuedures = baseProcedure.use(async ({ ctx, next }) => {
+  const headers = await getHeaders();
+  const session = await ctx.db.auth({ headers });
+
+  if (!session.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Not Authenticated",
+    });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      session: {
+        ...session,
+        user: session.user,
+      },
+    },
+  });
 });
