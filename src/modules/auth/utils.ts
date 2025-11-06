7148554300b6a12e0c75f74f53d54a339c48c8a2
omkar <omkarpadalkar21@gmail.com>
2025-11-07 02:21:36 +0530

    Fix cookie logic preventing login on localhost

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -12,8 +12,11 @@ export const generateAuthCookie = async ({ prefix, value }: Props) => {
     value: value,
     httpOnly: true,
     path: "/",
-    sameSite: "none",
-    domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-    secure: process.env.NODE_ENV === "production",
+    //enables cookie auth on localhost, but it won't work with subdomains turned on
+    ...(process.env.NODE_ENV !== "development" && {
+      sameSite: "none",
+      domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+      secure: process.env.NODE_ENV === "production",
+    }),
   });
 };
